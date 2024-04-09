e4537b2015564f38d13a7a203d949b56fcab6455
Cybwan <baili@flomesh.io>
2024-04-09 14:46:38 +0800

    update connector demo.

---
+++ scripts/deploy-fsm.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+fsm_namespace="${fsm_namespace:-fsm-system}"
+fsm_mesh_name="${fsm_mesh_name:-fsm}"
+
+fsm_cluster_name="${fsm_cluster_name:-fsm}"
+
+dns_svc_ip="$(kubectl get svc -n kube-system -l k8s-app=kube-dns -o jsonpath='{.items[0].spec.clusterIP}')"
+clusters="${clusters:-c0}"
+
+fsm install \
+    --mesh-name "$fsm_mesh_name" \
+    --fsm-namespace "$fsm_namespace" \
+    --set=fsm.image.registry=localhost:5000/flomesh \
+    --set=fsm.image.tag=latest \
+    --set=fsm.image.pullPolicy=Always \
+    --set=fsm.sidecar.sidecarLogLevel=debug \
+    --set=fsm.controllerLogLevel=warn \
+    --set=fsm.serviceAccessMode=mixed \
+    --set=fsm.featureFlags.enableAutoDefaultRoute=true \
+    --set=clusterSet.region=LN \
+    --set=clusterSet.zone=DL \
+    --set=clusterSet.group=FLOMESH \
+    --set=clusterSet.name="$fsm_cluster_name" \
+    --set fsm.fsmIngress.enabled=false \
+    --set fsm.fsmGateway.enabled=true \
+    --set=fsm.localDNSProxy.enable=true \
+    --set=fsm.localDNSProxy.wildcard.enable=false \
+    --set=fsm.localDNSProxy.primaryUpstreamDNSServerIPAddr=$dns_svc_ip \
+    --set fsm.featureFlags.enableValidateHTTPRouteHostnames=false \
+    --set fsm.featureFlags.enableValidateGRPCRouteHostnames=false \
+    --set fsm.featureFlags.enableValidateTLSRouteHostnames=false \
+    --set fsm.featureFlags.enableValidateGatewayListenerHostname=false \
+    --set fsm.featureFlags.enableGatewayProxyTag=true \
+    --set=fsm.featureFlags.enableSidecarPrettyConfig=true \
+    --timeout=900s
