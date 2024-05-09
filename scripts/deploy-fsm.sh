a366fb50a8e9f7c7d208461e914a3a911b8c9de7
Cybwan <baili@flomesh.io>
2024-05-09 20:15:42 +0800

    update connector demo.

--- scripts/deploy-fsm.sh
+++ scripts/deploy-fsm.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 CTR_REGISTRY="${CTR_REGISTRY:-flomesh}"
-CTR_TAG="${CTR_TAG:-1.2.4}"
+CTR_TAG="${CTR_TAG:-1.2.5-alpha.4}"
 IMAGE_PULL_POLICY="${IMAGE_PULL_POLICY:-Always}"
 
 fsm_namespace="${fsm_namespace:-fsm-system}"
